afc44495cfcf1c7c7578e3222a1817b58d570249
Konstantin Bogdanov <kbogdanov1@gmail.com>
2016-02-27 04:23:32 +0300

    third gallery added

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,61 @@ jQuery(document).ready(function($) {
     itemSelector: '.masonry__item'
   });
 
+
+	var $aside = $('aside');
+
+	$('#hamburger').on('click',function(e) {
+	    e.preventDefault();	
+	    $aside.animate({
+	    	right: parseInt($aside.css('right'),10) == 0 ? -$aside.outerWidth() : 0,
+	    },{
+	    	duration: 1500,
+	    	easing: 'easeOutQuint'
+	    });
+	});
+	$('#page-sidebar__close').on('click',function(e) {
+	    e.preventDefault();	
+	    $aside.animate({
+	    	right: -$aside.outerWidth(),
+	    },{
+	    	duration: 1500,
+	    	easing: 'easeOutQuint'
+	    });
+	});
+
+
+$('#mainGallery').slick({
+  arrows: true,
+  appendArrows: $('#mainGalleryTabs'),  
+  dots: true,
+  appendDots: $('#mainGalleryTabs'),  
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,  
+  centerMode: true,   
+  centerPadding: '0px',
+  variableWidth: true, 
+  slidesToShow: 2
+});
+
+
+
+$('#thirdGallery').slick({
+  arrows: true,
+  appendArrows: $('#thirdGalleryTabs'),  
+  dots: true,
+  appendDots: $('#thirdGalleryTabs'),  
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,  
+  slidesToShow: 1
+});
+
+
+
+
+
+
+
+
 });
