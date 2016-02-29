1f4b6e3d69167df8befd3252ca5f288fae4dc266
Konstantin Bogdanov <kbogdanov1@gmail.com>
2016-02-29 04:25:14 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -10119,12 +10119,43 @@ $('#thirdGallery').slick({
 
 
 
-var S="http://xn--80axfis.xn----7sbajzfbacq2eog.xn--p1ai/cgi/send-form.exe?"
-  S+="649c395d220f497ab3b4ef461bdf365c";
-  document.write("<form action=&quot;"+S+"&quot; … >");
 
+});
+$(document).ready(function() {
 
+	$("#feedback").submit(function(event) {
+		event.preventDefault();
 
+		var formData = new FormData($(this)[0]); //get data
+
+		$(this).find("button").hide(); //hide submit button
+		$('.form-notif').html("<span class='notification-load'>Sending...</span>"); //show send message
+
+		// ajax body
+		$.ajax({
+			type: "POST",
+			url: "feedback.php", //handler
+			data: formData,
+			contentType: false,
+			processData: false,
+			success: function(msg) {
+
+				if(msg == 'OK') {
+					// if send success then show message
+					result = '<span class="notification-success">Your mail was sent successfully!</span>'; //success message
+					$(".notification-load").hide(); //hide load message
+					$('.form-notif').html(result); //show success message
+					$(this).find("button"); //show submit button
+				} else {
+					// if send error then show error message
+					result = msg;
+					$(this).find("button"); //show submit button
+					$('.form-notif').html(result); //show error message
+				}
+			}
+		});
+		return false;
+	});
 
 });
 /*
