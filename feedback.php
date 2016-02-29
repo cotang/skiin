46bd23a78e1e6d6dd669a705aced936a93aaa735
Konstantin Bogdanov <kbogdanov1@gmail.com>
2016-02-29 18:53:32 +0300

    Updates

--- feedback.php
+++ feedback.php
@@ -7,16 +7,16 @@ $post = (!empty($_POST)) ? true : false;
 
 // if true then get POST data
 if($post) {
-	if ($_POST["name"]) $name = htmlspecialchars($_POST["name"]);
+	// if ($_POST["name"]) $name = htmlspecialchars($_POST["name"]);
 	if ($_POST["email"]) $email = htmlspecialchars($_POST["email"]);
-	if ($_POST["text"]) $text = htmlspecialchars($_POST["text"]);
+	// if ($_POST["text"]) $text = htmlspecialchars($_POST["text"]);
 	$error = "";
 
 	// check required fields
 	// if empty then add error message
-	if(!$name) {
-		$error .= "Please write name<br>";
-	}
+	// if(!$name) {
+	// 	$error .= "Please write name<br>";
+	// }
 	if(!$email) {
 		$error .= "Please write E-Mail<br>";
 	}
@@ -25,22 +25,22 @@ if($post) {
 	if(!$error) {
 		$mail = new PHPMailer(); // init class PHPMailer
 		$mail->From = $email; // e-mail from
-		$mail->FromName = $name; // name from
+		// $mail->FromName = $name; // name from
 		$mail->AddAddress('warlocked@mail.ru', 'Konstantin Bogdanov'); // e-mail and name to
 		$mail->IsHTML(true); // HTML format for mail
 		$mail->Subject = "The correspondence from SKIIN"; // mail subject
 
 		// if attached file then add this
-		if(isset($_FILES['file'])) {
-			if($_FILES['file']['error'][0] == 0){
-				$mail->AddAttachment($_FILES['file']['tmp_name'][0], $_FILES['file']['name'][0]);
-			}
-		}
+		// if(isset($_FILES['file'])) {
+		// 	if($_FILES['file']['error'][0] == 0){
+		// 		$mail->AddAttachment($_FILES['file']['tmp_name'][0], $_FILES['file']['name'][0]);
+		// 	}
+		// }
 
 		// mail body
 		$message = "<strong>E-mail</strong>: ".$email."<br>";
-		$message .= "<strong>Name</strong>: " .$name."<br>";
-		$message .= "<strong>Text</strong>:<br>".$text."<br>";
+		// $message .= "<strong>Name</strong>: " .$name."<br>";
+		// $message .= "<strong>Text</strong>:<br>".$text."<br>";
 		$mail->Body = $message;
 
 		// send mail
